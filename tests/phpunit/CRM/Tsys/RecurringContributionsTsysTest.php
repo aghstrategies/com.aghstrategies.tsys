e9fa4ad858cdb7d89f931c4369873657d9c5d100
Alice Frumin <alice@aghstrategies.com>
2019-03-12 18:04:58 -0400

    working creating a contribution with a vault token

--- tests/phpunit/CRM/Tsys/RecurringContributionsTsysTest.php
+++ tests/phpunit/CRM/Tsys/RecurringContributionsTsysTest.php
@@ -50,4 +50,38 @@ class CRM_Tsys_ContributionTsysTest extends CRM_Tsys_BaseTest {
     $results['vault_token'] = $paymentToken['token'];
     $this->spitOutResults('MerchantWARE 4.5 34.00 M', $results);
   }
+
+  /**
+  * MerchantWARE 4.5 38.00 M
+  */
+  public function testCayanCertificationScriptMerchantWare38M() {
+    $this->setupTransaction();
+    $recurringContribution = $this->createRecurringContribution();
+    $params = [
+      'amount' => 10.00,
+      'credit_card_number' => '4012000033330026',
+      'is_recur' => 1,
+      'contributionRecurID' => $recurringContribution['id'],
+    ];
+    $results = $this->doPayment($params);
+    $results['financial_type_id'] = 1;
+    $results['total_amount'] = $results['amount'];
+    $results['contact_id'] = $results['contactID'];
+
+    $firstContribution = civicrm_api3('Contribution', 'create', $results);
+
+    $this->assertEquals($results['trxn_result_code'], 'NC1000');
+    $this->assertEquals($results['payment_status_id'], $this->_completedStatusID);
+    $this->assertGreaterThan(0, $results['payment_token_id']);
+
+    $paymentToken = civicrm_api3('PaymentToken', 'getsingle', [
+      'id' => $results['payment_token_id'],
+    ]);
+    $results['contribution_recur_id'] = $recurringContribution['id'];
+    $results['vault_token'] = $paymentToken['token'];
+    $results['payment_processor'] =  $results['payment_processor_id'];
+    $results['receive_date'] = "2009-07-01 11:53:50";
+    CRM_Tsys_Recur::processContributionPayment($results, array(), $firstContribution['id']);
+    $this->spitOutResults('MerchantWARE 4.5 38.00 M', $results);
+  }
 }
