26209c3cfac0200af8dbb045638ba710a4b225c0
Alice Frumin <alice@aghtrategies.com>
2019-10-29 15:44:31 -0400

    debug make transaction variable

--- CRM/Core/Payment/Tsys.php
+++ CRM/Core/Payment/Tsys.php
@@ -359,13 +359,13 @@ private $_islive = FALSE;
    * @return array $params           payment params updated to inculde relevant info from Tsys
    */
   public static function processTransaction($makeTransaction, &$params, $tsysCreds) {
-    $params = self::processResponseFromTsys($params, $makeTransaction);
     $failedStatusId = CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Failed');
 
     // If transaction approved
     if (!empty($makeTransaction->Body->SaleResponse->SaleResult->ApprovalStatus)
     && $makeTransaction->Body->SaleResponse->SaleResult->ApprovalStatus == "APPROVED"
     && !empty($makeTransaction->Body->SaleResponse->SaleResult->Token)) {
+      $params = self::processResponseFromTsys($params, $makeTransaction);
       // Successful contribution update the status and get the rest of the info from Tsys Response
       $completedStatusId = CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Completed');
       $params['payment_status_id'] = $completedStatusId;
@@ -403,6 +403,7 @@ private $_islive = FALSE;
       }
 
       $errorMessage = self::handleErrorNotification($errorMessage, $params['tsys_error_url']);
+      CRM_Core_Error::debug_var('maketransaction', $makeTransaction);
       throw new \Civi\Payment\Exception\PaymentProcessorException('Failed to create TSYS Charge: ' . $errorMessage);
     }
   }
