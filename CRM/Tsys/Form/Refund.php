23ab6aed8175eda3e98a72d51134abec9ff41d28
Alice Frumin <alice@aghtrategies.com>
2020-02-03 16:07:23 -0500

    cleanup

--- CRM/Tsys/Form/Refund.php
+++ CRM/Tsys/Form/Refund.php
@@ -21,11 +21,9 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
 
     $this->_values = civicrm_api3('FinancialTrxn', 'getsingle', ['id' => $this->_id]);
 
-
+    // If the payment was run thru TSYS check if it should be voided (has not been batched yet) or Refunded (has been batched)
     $tsysProcessors = CRM_Core_Payment_Tsys::getAllTsysPaymentProcessors();
     if (!empty($this->_values['payment_processor_id']) && !in_array($this->_values['payment_processor_id'], $tsysProcessors)) {
-      // TODO Check Refund Amount -- 'RefundMaxAmount' is showing up as 0 so this is not very useful --
-      // unless these transactions need to be voided not refunded
       $tsysCreds = CRM_Core_Payment_Tsys::getPaymentProcessorSettings($this->_values['payment_processor_id']);
       $tsysInfo = CRM_Tsys_Soap::composeCheckBalanceSoapRequest($this->_values['trxn_id'], $tsysCreds);
       $response = $tsysInfo->Body->DetailedTransactionByReferenceResponse->DetailedTransactionByReferenceResult;
@@ -42,6 +40,7 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
         }
       }
     }
+    // If the payment was not run thru TSYS bounce
     else {
       CRM_Core_Error::statusBounce(ts('You cannot update this payment as it is not tied to a TSYS payment processor'));
     }
@@ -49,9 +48,10 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
 
   public function buildQuickForm() {
     $defaults = [];
-    // add form elements
-    // Documentation on AddMoney => https://github.com/civicrm/civicrm-core/blob/3329ccb30f7dab40ed0f3aa85ff30dff6901c8da/CRM/Core/Form.php#L1906
+
+    // If the transaction has been batched and thus should be refunded
     if ($this->actionAvailable == 'Refund') {
+      // Documentation on AddMoney => https://github.com/civicrm/civicrm-core/blob/3329ccb30f7dab40ed0f3aa85ff30dff6901c8da/CRM/Core/Form.php#L1906
       $this->addMoney(
         'refund_amount',
         E::ts('Amount to Refund'),
@@ -75,11 +75,12 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
       if (isset($this->maxRefundAmount)) {
         $this->add('hidden','max_refund_amount', $this->maxRefundAmount);
         $defaults['refund_amount'] = $this->maxRefundAmount|crmMoney;
-        CRM_Core_Session::setStatus(E::ts('Amount Available to Refund: $%1. Submitting this form will result in a refund being issued from your TSYS payment procesor.', array(
+        CRM_Core_Session::setStatus(E::ts('Amount Available to Refund for this payment is $%1. Submitting this form will result in a refund being issued from your TSYS payment processor.', array(
           1 => $this->maxRefundAmount,
         )), '', 'no-popup');
       }
     }
+    // If payment has not been batched and thus should be voided
     elseif ($this->actionAvailable == 'Void') {
       $this->addMoney(
         'refund_amount',
@@ -101,11 +102,10 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
         ),
       ));
 
-      $buttonName = E::ts('Void Payment');
       $this->add('hidden','payment_id', $this->_id);
       if (!empty($this->_values['total_amount'])) {
         $defaults['refund_amount'] = $this->_values['total_amount'];
-        CRM_Core_Session::setStatus(E::ts('This transaction has not been settled it is recommended you void the full amount $%1. Submitting this form will result in this transaction being voided via your TSYS payment procesor.', array(
+        CRM_Core_Session::setStatus(E::ts('This transaction has not been settled and so it is recommended that you void the full amount $%1 instead of refunding it. Submitting this form will result in this transaction being voided via your TSYS Payment processor.', array(
           1 => $this->_values['total_amount'],
         )), '', 'no-popup');
       }
@@ -128,14 +128,13 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
 
   public function postProcess() {
     $values = $this->exportValues();
-    // Get tsys credentials ($params come from a form)
+    // Get tsys credentials
     if (!empty($values['payment_processor_id'])
       && !empty($values['refund_amount'])
       && !empty($values['trxn_id']
       && !empty($values['formaction']))
     ) {
       $tsysCreds = CRM_Core_Payment_Tsys::getPaymentProcessorSettings($values['payment_processor_id']);
-
       if (!empty($tsysCreds)) {
         if ($values['formaction'] == 'Refund') {
           $runRefund = CRM_Tsys_Soap::composeRefundCardSoapRequest($values['trxn_id'], $values['refund_amount'], $tsysCreds);
@@ -145,7 +144,6 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
           $voidResponse = CRM_Tsys_Soap::composeVoidSoapRequest($values['trxn_id'], $tsysCreds);
           self::processVoidResponse($voidResponse, $values);
         }
-
       }
     }
     parent::postProcess();
@@ -158,9 +156,6 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
    * @return
    */
   public function processRefundResponse($runRefund, $values) {
-    $text = '';
-    $title = '';
-    $type = 'no-popup';
     // We got a legible response!!
     if (isset($runRefund->Body->RefundResponse->RefundResult->ApprovalStatus)) {
       // Refund processed successfully in TSYS so update CiviCRM payment accordingly
@@ -193,31 +188,43 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
         }
 
         // Update the user everything went well
-        $text = 'Payment successfully refunded.';
-        $title = 'Refund Approved';
-        $type = 'success';
+        CRM_Core_Session::setStatus(
+          'Payment successfully refunded.',
+          E::ts('Refund Approved'),
+          'Success'
+        );
       }
       // Refund failed explicitly so retrieve error
       elseif (substr($runRefund->Body->RefundResponse->RefundResult->ApprovalStatus, 0, 6 ) == "FAILED") {
-        $title = E::ts('Refund Failed');
         $approvalStatus = explode(';', $runRefund->Body->RefundResponse->RefundResult->ApprovalStatus);
         if (count($approvalStatus) == 3) {
-          $text = E::ts('Error Code %1, %2', array(
-            1 => $approvalStatus[1],
-            2 => $approvalStatus[2],
-          ));
+          CRM_Core_Session::setStatus(
+            E::ts('Error Code %1, %2', array(
+              1 => $approvalStatus[1],
+              2 => $approvalStatus[2],
+            )),
+            E::ts('Refund Failed'),
+            'error'
+          );
         } else {
-          $text = $approvalStatus;
+          CRM_Core_Session::setStatus(
+            $approvalStatus,
+            E::ts('Refund Failed'),
+            'error'
+          );
         }
       }
     }
+
     // We did not get a legible response
     else {
-      $title = E::ts('Refund Failed');
-      $text = E::ts('Refund Response could not be found see logs for more details.');
+      CRM_Core_Session::setStatus(
+        E::ts('Refund Response could not be found see logs for more details.'),
+        E::ts('Refund Failed'),
+        'error'
+      );
       // TODO add debug logs
     }
-    CRM_Core_Session::setStatus($text, $title, $type);
   }
 
   /**
@@ -227,9 +234,6 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
    * @return
    */
   public function processVoidResponse($voidResponse, $values) {
-    $text = '';
-    $title = '';
-    $type = 'no-popup';
     $response = $voidResponse->Body->VoidResponse->VoidResult;
     // We got a legible response!!
     if (isset($response->ApprovalStatus)) {
@@ -265,31 +269,42 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
         self::calculateContributionStatus($values['refund_amount'], $values['contribution_id'], $cancelDate);
 
         // Update the user everything went well
-        $text = 'Payment successfully voided.';
-        $title = 'Payment Voided';
-        $type = 'success';
+        CRM_Core_Session::setStatus(
+          'Payment successfully refunded.',
+          E::ts('Refund Approved'),
+          'Success'
+        );
       }
       // Refund failed explicitly so retrieve error
       elseif (substr($response->ApprovalStatus, 0, 6 ) == "FAILED") {
-        $title = E::ts('Void Failed');
         $approvalStatus = explode(';', $response->ApprovalStatus);
         if (count($approvalStatus) == 3) {
-          $text = E::ts('Error Code %1, %2', array(
-            1 => $approvalStatus[1],
-            2 => $approvalStatus[2],
-          ));
+          CRM_Core_Session::setStatus(
+            E::ts('Error Code %1, %2', array(
+              1 => $approvalStatus[1],
+              2 => $approvalStatus[2],
+            )),
+            E::ts('Void Failed'),
+            'error'
+          );
         } else {
-          $text = $approvalStatus;
+          CRM_Core_Session::setStatus(
+            $approvalStatus,
+            E::ts('Void Failed'),
+            'error'
+          );
         }
       }
     }
     // We did not get a legible response
     else {
-      $title = E::ts('Void Failed');
-      $text = E::ts('Void Response could not be found see logs for more details.');
+      CRM_Core_Session::setStatus(
+        E::ts('Void Response could not be found see logs for more details.'),
+        E::ts('Void Failed'),
+        'error'
+      );
       // TODO add debug logs
     }
-    CRM_Core_Session::setStatus($text, $title, $type);
   }
 
   /**
@@ -317,7 +332,6 @@ class CRM_Tsys_Form_Refund extends CRM_Core_Form {
         case 'Completed':
           // If the contribution was completed and we just voided the one
           // payment that completed it we should mark the contribution as cancelled
-          // TODO test this
           if ($contribution['total_amount'] == $voidedAmount) {
             self::updateContributionStatus([
               'id' => $contributionId,
