8df4f1cc6f8b3beb759eb2a6789306781a0b76c0
Alice Frumin <alice@aghtrategies.com>
2020-03-05 13:51:18 -0500

    cleanup note

--- tsys.php
+++ tsys.php
@@ -6,7 +6,6 @@ use CRM_Tsys_ExtensionUtil as E;
 
 function tsys_civicrm_links($op, $objectName, $objectId, &$links, &$mask, &$values) {
   // Adds a refund link to each payment made thru TSYS with a status of completed
-  // TODO once PR https://github.com/civicrm/civicrm-core/pull/16401 has been accepted make this version of tsys require that version of CiviCRM
   if ($objectName == 'Payment' && $op == 'Payment.edit.action') {
     if (!empty($values['contribution_id'])) {
 
