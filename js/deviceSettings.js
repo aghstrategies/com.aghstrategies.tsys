1b8073c96500dbbb19f55e8d95b3cd03cec53bc7
Alice Frumin <alice@aghstrategies.com>
2020-05-07 18:33:46 -0400

    adding template files etc

---
+++ js/deviceSettings.js
@@ -0,0 +1,4 @@
+CRM.$(function ($) {
+  // Moves device settings above save button
+  $('.deviceTable').insertBefore('.crm-submit-buttons:last');
+});
---
+++ templates/deviceButtons.tpl
@@ -0,0 +1,3 @@
+<td>{$form.device_1.html}</td>
+<td>{$form.device_2.html}</td>
+<td>{$form.device_3.html}</td>
