bc40d1691c15d7dd2e74ed30fb90819c18923649
Alice Frumin <alice@aghstrategies.com>
2020-05-14 15:21:34 -0400

    only show tsys device s if its a TSYS processor

--- js/deviceSettings.js
+++ js/deviceSettings.js
@@ -1,4 +1,15 @@
 CRM.$(function ($) {
   // Moves device settings above save button
   $('.deviceTable').insertBefore('.crm-submit-buttons:last');
+
+  // Show Device Table ONLY if TSYS processor
+  function checkProcessorType() {
+    if ($("select#payment_processor_type_id option:selected").text() == "TSYS") {
+      $('.deviceTable').show();
+    }
+    else {
+      $('.deviceTable').hide();
+    }
+  }
+  checkProcessorType();
 });
