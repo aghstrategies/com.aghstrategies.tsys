4e3f1e31d939a9661b95f4b09c27f021c61805b2
Mathieu Lu <mathieu@symbiotic.coop>
2024-08-20 14:27:58 -0400

    Upgrade civix (and bump min-req 5.65)

--- tsys.php
+++ tsys.php
@@ -469,24 +469,6 @@ function tsys_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function tsys_civicrm_postInstall() {
-  _tsys_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function tsys_civicrm_uninstall() {
-  _tsys_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
@@ -496,41 +478,12 @@ function tsys_civicrm_enable() {
 }
 
 /**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function tsys_civicrm_disable() {
-  _tsys_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function tsys_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _tsys_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
  * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function tsys_civicrm_config(&$config) {
   _tsys_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function tsys_civicrm_entityTypes(&$entityTypes) {
-  _tsys_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
